566e212bb886774c21a05a683d74ea92359fb004
vbordalo <36831295+vbordalo@users.noreply.github.com>
2021-06-05 12:24:11 -0400

    test correct

--- correct_history.sh
+++ correct_history.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 git filter-branch --env-filter '
-OLD_EMAIL="36831295+vbordalo@users.noreply.github.com"
+OLD_EMAIL1="vinicius@biotonix.com"
+OLD_EMAIL2="36831295+vbordalo@users.noreply.github.com"
 CORRECT_NAME="vbordalo"
 CORRECT_EMAIL="vinicius.bordalo@gmal.com"
-if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL1" ] or [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL2" ]
 then
     export GIT_COMMITTER_NAME="$CORRECT_NAME"
     export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
 fi
-if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ] or [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL2" ]
 then
     export GIT_AUTHOR_NAME="$CORRECT_NAME"
     export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
